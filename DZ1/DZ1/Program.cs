220b2b2ea8f3250c8bca012aeb06de246637d576
ChugaevMark <chugaev-mark@inbox.ru>
2021-09-11 00:47:29 +0300

    задача 1

---
+++ DZ1/DZ1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DZ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число К: ");
+            int k;
+            while (true) {
+                k = int.Parse(Console.ReadLine());
+                if (k > 365 || k < 1)
+                    Console.WriteLine("Введите число от 1 до 365 ");
+                else
+                    break;
+            }
+            int o = k % 7;
+            Console.WriteLine($"Номер дня недели {o}");
+        }
+    }
+}
